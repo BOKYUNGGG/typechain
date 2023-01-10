e78c24389057af0b9e4b04f3a4ad4317f5df4c72
Bo Kyung <113874167+BOKYUNGGG@users.noreply.github.com>
2023-01-10 21:40:46 +0900

    2023-1-10

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import crypto from "crypto"
+import { isDataView } from "util/types"
 
 interface BlockShape {
     hash : string,
@@ -6,7 +7,6 @@ interface BlockShape {
     height : number,
     data : string
 }
-
 
 class Block implements BlockShape {
     public hash : string
@@ -21,6 +21,38 @@ class Block implements BlockShape {
 
     static calculate(preHash : string, height : number, data : string):string{
         const toHash = `${preHash}${height}${data}`
-        return toHash
+
+        return crypto.createHash("sha256").update(toHash).digest('hex')
     }
 }
+
+class BlockChain {
+    private blocks : Block[]
+
+    constructor (){
+        this.blocks = []
+    }
+    private getPrevHash() : string{
+        if(this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length -1].hash
+    }
+    public addBlock(data : string){
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data)
+        this.blocks.push(newBlock)
+    }
+    public getBlocks(){
+
+        // prevent Hacking!!
+        return [...this.blocks]
+    }
+}
+
+const blockChain = new BlockChain()
+blockChain.addBlock('First one')
+blockChain.addBlock('Second one')
+blockChain.addBlock('Third one')
+
+// This will not be added in the block chain
+blockChain.getBlocks().push(new Block("xxxx", 23, "HACKEDDD"))
+
+console.log(blockChain.getBlocks())
